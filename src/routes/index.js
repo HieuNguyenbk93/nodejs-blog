eaee4fd390c4b0cbd06ef7a182d078133cecfc4d
Hieu Nguyen <hieunguyenbk93@gmail.com>
2024-10-25 08:14:29 +0700

    refactor code mvc

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,18 @@
 const newsRouter = require('./news');
+const siteRouter = require('./site')
 
 function route(app) {
-    app.get('/', (req, res) => {
-        return res.render('home');
-    });
-    
-    // app.get('/news', (req, res) => {
-    //     return res.render('news');
-    // });
     app.use('/news', newsRouter);
+    app.use('/', siteRouter)
     
-    app.get('/search', (req, res) => {
-        return res.render('search');
-    });
+    // app.get('/search', (req, res) => {
+    //     return res.render('search');
+    // });
     
-    app.post('/search', (req, res) => {
-        console.log(req.body);
-        return res.send('');
-    });
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     return res.send('');
+    // });
 }
 
 module.exports = route;
