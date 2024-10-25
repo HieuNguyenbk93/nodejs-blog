d991d181015f022088c577bd26bd87294db7fd61
Hieu Nguyen <hieunguyenbk93@gmail.com>
2024-10-25 10:11:47 +0700

    mongodb

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,28 @@
+const Course = require('../models/Course');
+
 class SiteController {
 
     // [GET] /home
     index (req, res) {
-        res.render('home');
+        try {
+            const getData = async function () {
+                const data = await Course.find({});
+                console.log(data);
+                res.json(data);
+            }
+            getData();
+        }
+        catch (err) {
+            res.status(400).json({error: 'ERROR!!!'});
+        }
+        
+        // Course.find({}, function (err, courses) {
+        //     if (!err) {
+        //         res.json(courses)
+        //     } else
+        //     res.status(400).json({error: 'ERROR!!!'});
+        // });
+        // res.render('home');
     }
 
     // [GET] /search
